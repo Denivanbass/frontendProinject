99281ebdbc6c0901eb2387697d04a2083677d65d
Denivan Dias <denivanbass@gmail.com>
2026-08-28 00:28:37 -0300

    refactor: Alteracao da baseURL api

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const api = axios.create( { 
     // baseURL: 'http://localhost:3333',
-      baseURL: 'https://mediumorchid-antelope-605245.hostingersite.com/',
+      baseURL: 'https://blanchedalmond-lark-299301.hostingersite.com/',
     headers: {
         'Content-Type': 'application/json'
     }
